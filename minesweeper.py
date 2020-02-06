dfdabecd0dba467a17e12a5bac5b142aa4bd2bfd
Jakub Chojak <abukkajohc@gmail.com>
2020-02-06 21:30:28 +0100

    added game mechanics

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,7 @@
 import random
 
+bombs_left = 0
+
 def fill_board_with_numbers_of_nearby_bombs(board_get, x, y):
     for rows in range(x):
         for cols in range(y):
@@ -107,14 +109,16 @@ def fill_board_with_numbers_of_nearby_bombs(board_get, x, y):
     return board_get
 
 def generate_board(x = 16, y = 16):
+    global bombs_left
     board = []
     for rows in range(x):
         board.append([])
         for cols in range(y): #0 means no bomb in nearby 1 - 1 etc. -1 means it is a bomb
-            if random.randint(0, 100) > 20:
+            if random.randint(0, 100) > 12:
                 board[rows].append(0)
             else:
                 board[rows].append(-1)
+                bombs_left += 1
     return fill_board_with_numbers_of_nearby_bombs(board, x, y)
 
 def gen_empty_tab(x = 16, y = 16):
@@ -135,13 +139,23 @@ def show_map(tab, board, allmode = False, x = 16, y = 16):
                     print("*", end = " ")
                 else:
                     print(board[rows][cols], end = " ")
+            elif tab[rows][cols] == 2:
+                print("^", end = " ") #flag
             else:
                 print("-", end = " ")
         print("")
 
 def main_loop(board):
+    global bombs_left
     sim_board = gen_empty_tab()
     while True:
+        if bombs_left == 0:
+            print("Wygrana!")
+            break
+        
+        print("Pozostało {} bomb.".format(bombs_left))
+        what_to_do = int(input("Co zrobic? [0]Oznacz, [1]Pokaz "))
+        
         choice = str(input("Wybierz pole... "))
         choice = choice.lower()
         tmp = ord(choice[0]) - ord('a')
@@ -149,14 +163,20 @@ def main_loop(board):
             tmp_num = int(choice[1]) * 10 + int(choice[2])
         else:
             tmp_num = int(choice[1])
-         
-        if board[tmp][tmp_num] == -1:
-            sim_board[tmp][tmp_num] = 1
-            print("Przegrana!")
-            show_map(sim_board, board, True)
-            break
-        else:
-            sim_board[tmp][tmp_num] = 1 #marked as read
+
+        if what_to_do == 0:
+            if board[tmp][tmp_num] == -1:
+                bombs_left -= 1
+                sim_board[tmp][tmp_num] = 2 #flagged
             show_map(sim_board, board)
+        if what_to_do == 1:    
+            if board[tmp][tmp_num] == -1:
+                sim_board[tmp][tmp_num] = 1
+                print("Przegrana!")
+                show_map(sim_board, board, True)
+                break
+            else:
+                sim_board[tmp][tmp_num] = 1 #marked as read
+                show_map(sim_board, board)
             
 main_loop(generate_board())
